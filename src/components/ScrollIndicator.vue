e1bd69e7641cee40d03f75894bfa624b324d6eb7
alexvoste <meshoff@proton.me>
2026-06-02 18:57:54 +0000

    feat: add ScrollIndicator component

---
+++ src/components/ScrollIndicator.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="fixed left-4 top-1/2 -translate-y-1/2 hidden xl:block z-40">
+    <div class="flex flex-col gap-2">
+      <div class="w-px h-8 bg-[#EC8254]/30"></div>
+      <div class="text-[0.6rem] font-mono text-[#7a7563] -rotate-90 whitespace-nowrap tracking-wider">SCROLL</div>
+      <div class="w-px h-8 bg-[#EC8254]/30"></div>
+    </div>
+  </div>
+</template>
